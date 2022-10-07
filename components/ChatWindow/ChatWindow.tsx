fe73c3c3552d0f32afc7551e6308d973c140c7d2
Tashi Sherpa <tashisherpa455@gmail.com>
2022-10-07 16:19:03 -0400

    flexbox-ed message section, wip

--- components/ChatWindow/ChatWindow.tsx
+++ components/ChatWindow/ChatWindow.tsx
@@ -3,11 +3,19 @@ import styles from "./ChatWindow.module.scss";
 
 const ChatWindow = () => {
   return (
-    <div className={styles.container}>
+    <>
       <div className={styles.messageSection}>
-        <h1>Chat Window</h1>
+        <div className={styles.messageClassName}>
+          <h1>CLASS NAME</h1>
+        </div>
+        <div className={styles.messageDisplayMessage}>
+          <h1>CHAT MESSAGES HERE</h1>
+        </div>
+        <div className={styles.messageChatBox}>
+          <h1>CHAT BOX HERE</h1>
+        </div>
       </div>
-    </div>   
+    </> 
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next";
 import Head from "next/head";
+import ChatWindow from "../components/ChatWindow/ChatWindow";
 import styles from "../styles/Home.module.scss";
 import TextBar from "../components/TextBar/TextBar";
 
