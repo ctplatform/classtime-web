fc4aea64a780c800de2eab9160c22c1f6e9619e2
David Fong <dfong633@gmail.com>
2022-10-08 19:15:21 -0400

    feat: Completed a basic textbox. Can add more styling in the future.

--- components/TextBar/TextBar.tsx
+++ components/TextBar/TextBar.tsx
@@ -1,28 +1,30 @@
-import {useState} from "react";
-import SendIcon from '@mui/icons-material/Send';
-import styles from "./TextBar.module.scss"
+import { FormEvent, useState } from "react";
+import SendIcon from "@mui/icons-material/Send";
+import styles from "./TextBar.module.scss";
 
-const TextBar = ()  => {
+const TextBar = () => {
+  const [currentMessage, setCurrentMessage] = useState("");
+  //the textbar function, is there items that you would change
 
-    const [currentMessage, setCurrentMessage] = useState("");
+  const handleOnSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    setCurrentMessage("");
+  };
 
-    const handleOnSubmit = (e: any) => { //What should be the type of e, the textbar function, is there items that you would change
-        e.preventDefault();
-        setCurrentMessage("");
-    }  
+  return (
+    <div className={styles.container}>
+      <form onSubmit={handleOnSubmit}>
+        <input
+          type="text"
+          placeholder="Type your message..."
+          className={styles.input}
+          value={currentMessage}
+          onChange={(e) => setCurrentMessage(e.target.value)}
+        />
+        <SendIcon className={styles.icon} />
+      </form>
+    </div>
+  );
+};
 
-    return (
-        <div className={styles.container}>
-            <form onSubmit={handleOnSubmit}>
-                className={styles.input}
-                <input type="text" placeholder="Type your message..." 
-                value={currentMessage} 
-                onChange={(e) => setCurrentMessage(e.target.value)} 
-                />
-                <SendIcon />    
-            </form>
-        </div> 
-    )
-}
-
-export default TextBar
+export default TextBar;
