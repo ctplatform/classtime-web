19b91b72a005ed6710e0eff69d0e34c969703f56
skara9 <89281036+skara9@users.noreply.github.com>
2022-10-28 19:07:04 +0000

    feat: add color prop to course block

--- components/courseblock.tsx
+++ components/courseblock.tsx
@@ -4,16 +4,21 @@ function CourseBlock({
   courseName,
   section,
   type,
+  color,
 }: {
   courseName: string;
   section: number;
   type: string;
+  color: string;
 }) {
   return (
     <div className={styles.container}>
-      <div className={styles.highlight}></div>
+      <div
+        className={styles.highlight}
+        style={{ backgroundColor: color }}
+      ></div>
 
-      <div className={styles.main}>
+      <div className={styles.text}>
         <span className={styles.name}>{courseName}</span>
         <span className={styles.section}>Section {section}</span>
 
