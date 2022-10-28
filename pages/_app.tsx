cb3249eee1ba9df2371558656941e9ba2c9008b6
skara9 <89281036+skara9@users.noreply.github.com>
2022-10-28 19:27:55 +0000

    fix: undo changes to app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,8 @@
 import { AppProps } from "next/app";
-import CourseBlock from "../components/courseblock";
 import "../styles/globals.scss";
 
 function App({ Component, pageProps }: AppProps) {
-  const courseName = "Computer Science";
-  const sectionNum = 7;
-  const type = "Lecture";
-  const courseLength = 2;
-  const courseQuantity = 2;
-  const courseStart = new Date();
-  return (
-    <CourseBlock
-      courseName={courseName}
-      sectionNum={sectionNum}
-      type={type}
-      courseLength={courseLength}
-      courseQuantity={courseQuantity}
-      courseStart={courseStart}
-    />
-  );
+  return <Component {...pageProps} />;
 }
 
 export default App;
