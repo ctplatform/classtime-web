1cd45f5d7c571f8f149f5d37623c8572c516e700
JustinT9 <jus81818@yahoo.com>
2022-11-01 16:46:18 -0400

    update

---
+++ components/CourseBlock.tsx
@@ -0,0 +1,31 @@
+import styles from "./CourseBlock.module.scss";
+
+function CourseBlock({
+  courseName,
+  section,
+  blockType,
+  color,
+}: {
+  courseName: string;
+  section: number;
+  blockType: string;
+  color: string;
+}) {
+  return (
+    <div className={styles.container}>
+      <div
+        className={styles.highlight}
+        style={{ backgroundColor: color }}
+      ></div>
+
+      <div className={styles.text}>
+        <span className={styles.name}>{courseName}</span>
+        <span className={styles.section}>Section {section}</span>
+
+        <span className={styles.blocktype}>{blockType}</span>
+      </div>
+    </div>
+  );
+}
+
+export default CourseBlock;
