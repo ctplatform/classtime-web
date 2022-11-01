9bd0084438a6fba7114e355e0718afb5445a7962
Tashi Sherpa <tashisherpa455@gmail.com>
2022-11-01 17:07:59 -0400

    fixed icon based on first char of name, message component

--- components/ChatMessage/ChatMessage.tsx
+++ components/ChatMessage/ChatMessage.tsx
@@ -1,26 +1,28 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import styles from "./ChatMessage.module.scss";
 
 const ChatMessage = (props) => {
-    return (
-      <>
-        <div className={styles.chatMessageZAxis}>
-          <section className={styles.chatMessageComponent}>
-            <div className={styles.chatMessageBar1}>
-              <div className={styles.chatMessageAccIcon}></div>
+  return (
+    <>
+      <div className={styles.chatMessageZAxis}>
+        <section className={styles.chatMessageComponent}>
+          <div className={styles.chatMessageBar1}>
+            <div className={styles.chatMessageAccIcon}>
               {props.messageIcon}
+              <section className={styles.chatMessageIconChar}>
+                {props.messageName[0]}
+              </section>
             </div>
-            <div className={styles.chatMessageBar2}>
-              <h1 className={styles.chatMessageName}>{props.messageName}</h1>
-              <h2 className={styles.chatMessageTime}>{props.messageTime}</h2>
-              <p className={styles.chatMessageMessage}>
-                {props.messageText}
-              </p>
-            </div>
-          </section>
-        </div>
-      </>
-    );
-}
+          </div>
+          <div className={styles.chatMessageBar2}>
+            <h1 className={styles.chatMessageName}>{props.messageName}</h1>
+            <h2 className={styles.chatMessageTime}>{props.messageTime}</h2>
+            <p className={styles.chatMessageMessage}>{props.messageText}</p>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
 
 export default ChatMessage;
