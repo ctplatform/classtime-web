1fdc656c64a7a27a297d07e32cbde4108726d3b8
PhoenixTamaoki <Yonexman2@gmail.com>
2022-12-02 16:21:52 -0500

    first version of delete message

---
+++ pages/api/course-section/[id].ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../prisma/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") return res.status(405).end();
+
+  if (typeof req.query.id !== "string")
+    return res.status(400).end("Invalid Identifier");
+
+  const user = await prisma.user.findFirst({
+    where: { id: req.query.id },
+  });
+
+  res.status(200).json(user);
+}
