5dc94053762fc1084e8b12c7d68a4e6052b7ef78
PhoenixTamaoki <Yonexman2@gmail.com>
2022-12-02 16:38:54 -0500

    delete msg file

--- pages/api/course-section/[id]/chat/messages/[id]/delete.ts
+++ pages/api/course-section/[id]/chat/messages/[id]/delete.ts
@@ -1,18 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import prisma from "../../../prisma/client";
+import prisma from "../../../../../../../prisma/client";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "GET") return res.status(405).end();
+  if (req.method !== "DELETE") return res.status(405).end();
 
   if (typeof req.query.id !== "string")
     return res.status(400).end("Invalid Identifier");
 
-  const user = await prisma.user.findFirst({
+  prisma.message.delete({
     where: { id: req.query.id },
   });
 
-  res.status(200).json(user);
+  res.status(200);
 }
