41ecb548d137be9c5b819ef59ec180f678746e38
lForger <navajaydan@gmail.com>
2022-12-02 16:31:40 -0500

    Added a first draft of a test for messages

--- test/api/user/[id].test.ts
+++ test/api/user/[id].test.ts
@@ -1,4 +1,4 @@
-import { User } from "@prisma/client";
+import { Message, User } from "@prisma/client";
 import { testApiHandler } from "next-test-api-route-handler";
 import { prismaMock } from "../../singleton";
 import handler from "../../../pages/api/user/[id]";
@@ -35,5 +35,21 @@ describe("/api/user/[id]", () => {
         expect(res.json()).resolves.toBeNull();
       },
     });
+  });
+});
+
+describe("/api/course-section/[id]/chat/[send]", () => {
+  it("it should return nothing/ a success status", async () => {
+    const id = "my_user_id";
+    const user = {
+      id,
+      firstName: "First",
+      lastName: "Last",
+    } as User;
+    const message = {
+      authorId: user.id,
+      chatRoomId: "my_chatroom_id",
+      content: "This is a message! ",
+    } as Message;
   });
 });
